b6859973ed2d2b9edd2b99cfc92a250b1c620da4
rachmadiantyy <rachmadiantyy@gmail.com>
2025-03-09 20:27:25 +0700

    add new api-key

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 # Memuat file .env ke dalam environment
 load_dotenv()
 
-API_KEY = os.getenv("OPENWEATHER_API_KEY", "a82c2af37ad2faf45c49462cb034994e")
+API_KEY = os.getenv("OPENWEATHER_API_KEY", "9e469a336cc7ed95387acd98babd13b2")
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
