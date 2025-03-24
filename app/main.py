f7baf5a236eafbfbd1b84467e8536f3a166aedaf
rachmadiantyy <rachmadiantyy@gmail.com>
2025-03-24 13:53:23 +0700

    Revert "add for setup prometeus"
    This reverts commit 333295767569376955f72fb377d38304eb63ac20.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI
-from .routes import router  # Import routes dari routes.py
+from .routes import router  # Import routes from routes.py
 from .config import API_KEY, BASE_URL  # Sesuaikan import sesuai kebutuhan
-from prometheus_fastapi_instrumentator import Instrumentator  # Tambahkan ini
+
 
 app = FastAPI()
-
-# Tambahkan middleware Prometheus untuk monitoring
-Instrumentator().instrument(app).expose(app)
 
 # Daftarkan routes
 app.include_router(router)
