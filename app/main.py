6d7e4a03b37bb38de05cd29a8785fcb55a28219d
rachmadiantyy <rachmadiantyy@gmail.com>
2025-03-24 13:46:40 +0700

    add for setup prometeus

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from .routes import router  # Import routes from routes.py
+from .routes import router  # Import routes dari routes.py
 from .config import API_KEY, BASE_URL  # Sesuaikan import sesuai kebutuhan
-
+from prometheus_fastapi_instrumentator import Instrumentator  # Tambahkan ini
 
 app = FastAPI()
+
+# Tambahkan middleware Prometheus untuk monitoring
+Instrumentator().instrument(app).expose(app)
 
 # Daftarkan routes
 app.include_router(router)
