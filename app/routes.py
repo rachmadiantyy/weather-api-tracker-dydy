0afd88c58fd5466066dc7c802162e734f75ecc36
rachmadiantyy <rachmadiantyy@gmail.com>
2025-06-08 15:54:23 +0700

    fix

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, HTTPException
 from service import get_weather_data
 from models import WeatherResponse
 
-
 router = APIRouter()
 
 @router.get("/weather/{city}", response_model=WeatherResponse, operation_id="get_weather_by_city")
@@ -11,6 +10,9 @@ def get_weather(city: str):
     Mengambil data cuaca berdasarkan nama kota.
     """
     weather_data = get_weather_data(city)
+    
+    # Pastikan weather_data tidak None
     if weather_data:
         return weather_data
-    raise HTTPException(status_code=404, detail="Kota tidak ditemukan")
+    # Menangani jika data tidak ditemukan
+    raise HTTPException(status_code=404, detail="Kota tidak ditemukan atau data tidak valid")
